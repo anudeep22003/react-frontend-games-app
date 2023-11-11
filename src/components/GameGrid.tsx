d898f8acff622c6c37e88644e3e04c0d5dce8701
Anudeep Y <anudeep.yegireddi@gmail.com>
2023-11-12 00:39:10 +0530

    chore: cleaned up gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -28,32 +28,32 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  if (error) {
+    <Text color={"tomato"}>{error}</Text>;
+  }
+
   return (
-    <>
-      {error && <Text color={"tomato"}>{error}</Text>}
-      <SimpleGrid
-        columns={{
-          sm: 1,
-          md: 2,
-          lg: 3,
-          xl: 3,
-        }}
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <SkeletonCard />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid
+      columns={{
+        sm: 1,
+        md: 2,
+        lg: 3,
+        xl: 3,
+      }}
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <SkeletonCard />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-      {}
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
